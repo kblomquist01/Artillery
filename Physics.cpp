2abc0048c1e7097f6a77f37f94ed1f7edc814708
knblomquist00 <blo18004@byui.edu>
2023-10-30 16:03:47 -0600

    physics

--- Physics.cpp
+++ Physics.cpp
@@ -123,10 +123,10 @@ double findHorizontalComponent(double a, double total) {
     *    total : total component
     ***********************************************/
 
-//double findTotalComponent(double dx, double dy) {
-//    double total = (pow(dx, 2) + pow(dy, 2));
-//    return sqrt(total);
-//}
+double findTotalComponent(double dx, double dy) {
+    double total = (pow(dx, 2) + pow(dy, 2));
+    return sqrt(total);
+}
 
 /*************************************************
     * RADIANS FROM DEGEES
@@ -201,28 +201,33 @@ double findAreaOfCircle(double radius) {
 * Newton's second law of motion 
 * Return force 
 *******************************************/
-double lawOfMotion(double mass, double acceleration) {
-    return mass * acceleration;
+double lawOfMotion(double mass, double force) {
+    //f = m a
+    //f/ m = a
+    return force/ mass;
 }
+
 /*******************************************
 * Linear interpolation equation 
 * (r - r0) / (d - d0) = (r1 - r0) / (d1 - d0)
 * where d0,r0 are the coordinates of one point 
 * and d1,r1 are the coordinates of a second point 
 * and d,r are the coordinates of a point in the middle 
-* that you are trying to find 
-* Returns 
 *******************************************/
 double linearInterpolation(double d0, double r0, double d1, double r1, double r) {
     return d0 + (r - r0) * ((d1 - d0) / (r1 - r0));
 }
 
+/*******************************************
+* Find gravity from the altitude 
+* using linear interpolation 
+*******************************************/
 double findGravity(double altitude1) {
     double altitude[14] = {1, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 25000};
     double gravity[14] = {-9.807, -9.804, -9.801, -9.797, -9.794, -9.791, -9.788, -9.785, -9.782, -9.779, -9.776, -9.761, -9.745, -9.730 };
 
     for (int i = 1; i < 14; i++) {
-        if (altitude1 < 0.0) {
+        if (altitude1 <= 0.01) {
             return -9.807;
         }
         if (altitude[i] > altitude1) {
@@ -231,6 +236,68 @@ double findGravity(double altitude1) {
     }
 
     return gravity[13];
+}
+
+/*******************************************
+* Find speed of sound from the altitude
+* using linear interpolation
+*******************************************/
+double findSpeed(double altitude1) { // speed of sound 
+    double altitude[16] = { 0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 25000, 30000, 40000 };// in m
+    double speed[16] = {340, 336, 332, 328, 324, 320, 316, 312, 308, 303, 299, 295, 295, 295, 305, 324};//in m/s
+	
+    for (int i = 1; i < 16; i++) {
+		if (altitude1 <= 0.01) {
+			return 340;
+		}
+		if (altitude[i] > altitude1) {
+			return linearInterpolation(speed[i - 1], altitude[i - 1], speed[i], altitude[i], altitude1);
+		}
+	}
+
+	return speed[15];
+}
+
+/*******************************************
+* Find drag coefficient from Mach / speed of sound 
+* using linear interpolation
+*******************************************/
+double findDragCoefficient(double speed1) {
+    double mach[16] = {0.300, 0.500, 0.700, 0.890, 0.920, 0.960, 0.980, 1.000, 1.020, 1.060, 1.240, 1.530, 1.990, 2.870, 2.890, 5.000};
+    double coefficient[16] = { 0.1629, 0.1659, 0.2031, 0.2597, 0.3010, 0.3287, 0.4002, 0.4258, 0.4335, 0.4483, 0.4064, 0.3663, 0.2897, 0.2297, 0.2306, 0.2656 };
+	
+    for (int i = 1; i < 16; i++) {
+		if (speed1 <= 0.01) {
+			return 0.1629;
+		}
+		if (mach[i] > speed1) {
+			return linearInterpolation(coefficient[i - 1], mach[i - 1], coefficient[i], mach[i], speed1);
+		}
+	}
+
+	return coefficient[15];
+
+    return 0;
+}
+
+/*******************************************
+* Find air density from the altitude 
+* using linear interpolation
+*******************************************/
+double findDensity(double altitude1) {
+    double altitude[20] = { 0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 25000, 30000, 40000, 50000, 60000, 70000, 80000};
+    double density[20] = { 1.2250000, 1.1120000, 1.0070000, 0.9093000, 0.8194000, 0.7364000, 0.6601000, 0.5900000, 0.5258000, 0.4671000, 0.4135000, 0.1948000, 0.0889100, 0.0400800, 0.0184100, 0.0039960, 0.0010270, 0.0003097, 0.0000828, 0.0000185 };
+
+	for (int i = 1; i < 20; i++) {
+		if (altitude1 <= 0.01) {
+			return 1.2250000;
+		}
+		if (altitude[i] > altitude1) {
+			return linearInterpolation(density[i - 1], altitude[i - 1], density[i], altitude[i], altitude1);
+		}
+	}
+    
+    return density[19];
 }
 
 /*******************************************
@@ -253,26 +320,40 @@ int main() {
     float velocity = 827;
     float dx = findHorizontalComponent(angle, velocity);
     float dy = findVerticalComponent(angle, velocity);
-    double ddx = 0;
-    double ddy = 0;
-    double acceleration = 0;
-    double drag = 0;
+    double dragCoefficient = 0;
+    double mach = 0;
+    double dragx = 0.3;
+    double dragy = .3;
+    double airDensity = 0.6;
+    double surfaceArea = 3.14159 * (0.077445 * 0.077445);
     double gravity = -9.8;
 
     double distance = 0; // in meters 
     double hangTime = 0;
     
-    while(y > 0) {
+    while(y + dy/100> 0) {
 
         gravity = findGravity(y);
-        dy = computeVelocity(dy, gravity, .01);
-        x = computeDistance(x, dx, 0, .01);
-        y = computeDistance(y, dy, gravity, .01);
+        airDensity = (findDensity(y));
+
+        if (dx > 0) {
+            dragx = -findDrag(findDragCoefficient(dx / findSpeed(y)), airDensity, dx, surfaceArea);
+        }
+        else {
+            dragx = findDrag(findDragCoefficient(dx / findSpeed(y)), airDensity, dx, surfaceArea);
+        }
+        dragy = -findDrag(findDragCoefficient(dy / findSpeed(y)), airDensity, dy, surfaceArea);
+        dy = computeVelocity(dy, gravity + lawOfMotion(WEIGHT, dragy), .01);
+        dx = computeVelocity(dx, lawOfMotion(WEIGHT, dragx), .01);
+        x = computeDistance(x, dx, dragx, .01);
+        y = computeDistance(y, dy, dragy + gravity, .01);
 
         hangTime += .01;
-        
-        
+        //cout << gravity << "g " << airDensity << "ad " << dragx << "drx " << dragy << "dry " << dy << "dy " << dx << "dx " << x << "x " << y << "y\n";
     }
+
+    x = linearInterpolation(x, y, x + dx/100, y + dy/100, 0);
+    y = 0;
 
     cout << "distance: " << x << "m Altitude: " << y << "m Hang time: " << hangTime << "s";
 
