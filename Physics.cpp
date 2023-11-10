d611e93272f4d0d61640527abc31638790cc61a8
knblomquist00 <blo18004@byui.edu>
2023-11-10 13:57:53 -0700

    tests

--- Physics.cpp
+++ Physics.cpp
@@ -1,4 +1,4 @@
-//#include "Physics.h"
+#include "Physics.h"
 #include <iostream>  // for CIN and COUT
 using namespace std;
 #include <cmath>
@@ -20,7 +20,7 @@ using namespace std;
     *     s : new position, in meters
     **************************************************/
 
-double computeDistance(double s, double v, double a, double t) {
+double Physics::computeDistance(double s, double v, double a, double t) {
     return s + v * t + 0.5 * a * (t * t);
 }
 /**************************************************
@@ -35,7 +35,7 @@ double computeDistance(double s, double v, double a, double t) {
     *     a : acceleration, in meters/second^2
     ***************************************************/
 
-double computeAcceleration(double f, double m)
+double Physics::computeAcceleration(double f, double m)
 {
     return f / m;
 }
@@ -55,7 +55,7 @@ double computeAcceleration(double f, double m)
     *     v : new velocity, in meters/second
     ***********************************************/
 
-double computeVelocity(double v, double a, double t) {
+double Physics::computeVelocity(double v, double a, double t) {
     return v + a * t;
 }
 
@@ -78,7 +78,7 @@ double computeVelocity(double v, double a, double t) {
     *     y : the vertical component of the total
     ***********************************************/
 
-double findVerticalComponent(double a, double total) {    
+double Physics::findVerticalComponent(double a, double total) {
     return cos(a) * total;
 }
 /***********************************************
@@ -100,7 +100,7 @@ double findVerticalComponent(double a, double total) {
     *     x : the vertical component of the total
     ***********************************************/
 
-double findHorizontalComponent(double a, double total) {
+double Physics::findHorizontalComponent(double a, double total) {
     return sin(a) * total;
 }
 /************************************************
@@ -123,7 +123,7 @@ double findHorizontalComponent(double a, double total) {
     *    total : total component
     ***********************************************/
 
-double findTotalComponent(double dx, double dy) {
+double Physics::findTotalComponent(double dx, double dy) {
     double total = (pow(dx, 2) + pow(dy, 2));
     return sqrt(total);
 }
@@ -138,7 +138,7 @@ double findTotalComponent(double dx, double dy) {
     *     r : radians from 0 to 2pi
     **************************************************/
 
-double radiansFromDegrees(double d) {
+double Physics::radiansFromDegrees(double d) {
     return (d / 360) * 2 * 3.14159265;
 }
 
@@ -147,14 +147,14 @@ double radiansFromDegrees(double d) {
 * Drag Force Equation 
 * Find drag 
 *******************************************/
-double findDrag(double dragCoefficient, double density, double velocity, double area) {
+double Physics::findDrag(double dragCoefficient, double density, double velocity, double area) {
     return .5 * (dragCoefficient * density * (velocity * velocity) * area);
 }
 
 /*******************************************
 * Area of a circle 
 *******************************************/
-double findAreaOfCircle(double radius) {
+double Physics::findAreaOfCircle(double radius) {
     return 3.1415 * (radius * radius);
 }
 
@@ -165,7 +165,7 @@ double findAreaOfCircle(double radius) {
 * and d1,r1 are the coordinates of a second point 
 * and d,r are the coordinates of a point in the middle 
 *******************************************/
-double linearInterpolation(double d0, double r0, double d1, double r1, double r) {
+double Physics::linearInterpolation(double d0, double r0, double d1, double r1, double r) {
     return d0 + (r - r0) * ((d1 - d0) / (r1 - r0));
 }
 
@@ -173,7 +173,7 @@ double linearInterpolation(double d0, double r0, double d1, double r1, double r)
 * Find gravity from the altitude 
 * using linear interpolation 
 *******************************************/
-double findGravity(double altitude1) {
+double Physics::findGravity(double altitude1) {
     double altitude[14] = {1, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 25000};
     double gravity[14] = {-9.807, -9.804, -9.801, -9.797, -9.794, -9.791, -9.788, -9.785, -9.782, -9.779, -9.776, -9.761, -9.745, -9.730 };
 
@@ -193,7 +193,7 @@ double findGravity(double altitude1) {
 * Find speed of sound from the altitude
 * using linear interpolation
 *******************************************/
-double findSpeed(double altitude1) { // speed of sound 
+double Physics::findSpeed(double altitude1) { // speed of sound 
     double altitude[16] = { 0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 25000, 30000, 40000 };// in m
     double speed[16] = {340, 336, 332, 328, 324, 320, 316, 312, 308, 303, 299, 295, 295, 295, 305, 324};//in m/s
 	
@@ -213,7 +213,7 @@ double findSpeed(double altitude1) { // speed of sound
 * Find drag coefficient from Mach / speed of sound 
 * using linear interpolation
 *******************************************/
-double findDragCoefficient(double speed1) {
+double Physics::findDragCoefficient(double speed1) {
     double mach[16] =        {0.300,   0.500,  0.700,  0.890,  0.920,  0.960,  0.980,  1.000,  1.020,  1.060,  1.240,  1.530,  1.990,  2.870,  2.890,  5.000};
     double coefficient[16] = { 0.1629, 0.1659, 0.2031, 0.2597, 0.3010, 0.3287, 0.4002, 0.4258, 0.4335, 0.4483, 0.4064, 0.3663, 0.2897, 0.2297, 0.2306, 0.2656 };
 	
@@ -235,7 +235,7 @@ double findDragCoefficient(double speed1) {
 * Find air density from the altitude 
 * using linear interpolation
 *******************************************/
-double findDensity(double altitude1) {
+double Physics::findDensity(double altitude1) {
     double altitude[20] = { 0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 25000, 30000, 40000, 50000, 60000, 70000, 80000};
     double density[20] = { 1.2250000, 1.1120000, 1.0070000, 0.9093000, 0.8194000, 0.7364000, 0.6601000, 0.5900000, 0.5258000, 0.4671000, 0.4135000, 0.1948000, 0.0889100, 0.0400800, 0.0184100, 0.0039960, 0.0010270, 0.0003097, 0.0000828, 0.0000185 };
 
@@ -258,69 +258,74 @@ double findDensity(double altitude1) {
 * dx is the horizontal component of speed (m/s)
 * dy is the vertical component of speed (m/s)
 *******************************************/
-double findAngle(float dx, float dy)
+double Physics::findAngle(float dx, float dy)
 {
     return atan2(dx, dy);
 }
 
-
-int main() {
-    double angle = radiansFromDegrees(-75);
-    double x = 0;
-    double y = .25;
-    float velocity = 827;
-    float dx = findHorizontalComponent(angle, velocity);
-    float dy = findVerticalComponent(angle, velocity);
-    double dragCoefficient = 0;
-    double mach = 0;
-    double dragx = 0.3;
-    double dragy = .3;
-    double drag = 0;
-    double airDensity = 0.6;
-    double surfaceArea = 3.14159 * (0.077445 * 0.077445);
-    double gravity = -9.8;
-
-    double distance = 0; // in meters 
-    double hangTime = 0;
-    
-    while(y + dy/100> 0) {
-
-        gravity = findGravity(y);
-        airDensity = (findDensity(y));
-        angle = findAngle(dx, dy);
-        velocity = findTotalComponent(dx, dy);
-        mach = velocity / findSpeed(y);
-
-        dragCoefficient = findDragCoefficient(mach);
-
-        
-        drag = findDrag(dragCoefficient, airDensity, velocity, surfaceArea);
-        float acceleration = computeAcceleration(drag, WEIGHT);
-        float ddx = acceleration * -sin(angle);
-        float ddy = acceleration * -cos(angle) + gravity;
-
-        // computeAcceleration parameters are (force, then mass) 
-        dy = computeVelocity(dy, ddy, .01);
-        dx = computeVelocity(dx, ddx, .01);
-
-        x = computeDistance(x, dx, ddx, .01);
-        y = computeDistance(y, dy, ddy, .01);
-
-        hangTime += .01;
-        // << "Gravity: " << gravity << " X: " << x
-
-        cout << std::fixed << std::showpoint;
-        cout << std::setprecision(4);
-        cout << " airDensity: " << airDensity << " Drag: " << drag << " Velocity: " << velocity;
-        cout << " DY:  " << dy << " DX: " << dx  << " Y: " << y << " angle: " << angle;
-        cout << " Drag Coefficient: " << dragCoefficient << " Mach: " << mach << "\n";
-    }
-
-    x = linearInterpolation(x, y, x + dx/100, y + dy/100, 0);
-    y = 0;
-
-    cout << "distance: " << x << "m Altitude: " << y << "m Hang time: " << hangTime << "s";
-
-
-    return 0;
+double Physics::getHistory()
+{
+    return 0.0;
 }
+
+
+//int main() {
+//    double angle = radiansFromDegrees(-75);
+//    double x = 0;
+//    double y = .25;
+//    float velocity = 827;
+//    float dx = findHorizontalComponent(angle, velocity);
+//    float dy = findVerticalComponent(angle, velocity);
+//    double dragCoefficient = 0;
+//    double mach = 0;
+//    double dragx = 0.3;
+//    double dragy = .3;
+//    double drag = 0;
+//    double airDensity = 0.6;
+//    double surfaceArea = 3.14159 * (0.077445 * 0.077445);
+//    double gravity = -9.8;
+//
+//    double distance = 0; // in meters 
+//    double hangTime = 0;
+//    
+//    while(y + dy/100> 0) {
+//
+//        gravity = findGravity(y);
+//        airDensity = (findDensity(y));
+//        angle = findAngle(dx, dy);
+//        velocity = findTotalComponent(dx, dy);
+//        mach = velocity / findSpeed(y);
+//
+//        dragCoefficient = findDragCoefficient(mach);
+//
+//        
+//        drag = findDrag(dragCoefficient, airDensity, velocity, surfaceArea);
+//        float acceleration = computeAcceleration(drag, WEIGHT);
+//        float ddx = acceleration * -sin(angle);
+//        float ddy = acceleration * -cos(angle) + gravity;
+//
+//        // computeAcceleration parameters are (force, then mass) 
+//        dy = computeVelocity(dy, ddy, .01);
+//        dx = computeVelocity(dx, ddx, .01);
+//
+//        x = computeDistance(x, dx, ddx, .01);
+//        y = computeDistance(y, dy, ddy, .01);
+//
+//        hangTime += .01;
+//        // << "Gravity: " << gravity << " X: " << x
+//
+//        cout << std::fixed << std::showpoint;
+//        cout << std::setprecision(4);
+//        cout << " airDensity: " << airDensity << " Drag: " << drag << " Velocity: " << velocity;
+//        cout << " DY:  " << dy << " DX: " << dx  << " Y: " << y << " angle: " << angle;
+//        cout << " Drag Coefficient: " << dragCoefficient << " Mach: " << mach << "\n";
+//    }
+//
+//    x = linearInterpolation(x, y, x + dx/100, y + dy/100, 0);
+//    y = 0;
+//
+//    cout << "distance: " << x << "m Altitude: " << y << "m Hang time: " << hangTime << "s";
+//
+//
+//    return 0;
+//}
--- testPhysics.h
+++ testPhysics.h
@@ -20,6 +20,153 @@ This means that a few dozen unit tests will need to be created.
 
 class TestPhysics
 {
+public:
+	void run() {
 
+	}
+private:
+	void testComputeDistance() {
+		Physics phy;
 
+		//zero inputs
+		assert(phy.computeDistance(0,0,0,0) == 0.0);
+		//start postion test
+		assert(phy.computeDistance(1.0, 0, 0, 0) == 1.0);
+		//time only
+		assert(phy.computeDistance(0, 0, 0, 1.0) == 0.0);
+		//velocity no time
+		assert(phy.computeDistance(0, 1.0, 0, 0) == 0.0);
+		//velocity time
+		assert(phy.computeDistance(0, 1.0, 0, 1.0) == 1.0);
+		//velocity
+		assert(phy.computeDistance(0, 2.0, 0, 1.0) == 2.0);
+		//time to velocity
+		assert(phy.computeDistance(0, 1.0, 0, 2.0) == 2.0);
+		//acceleration no time
+		assert(phy.computeDistance(0, 0, 1.0, 0) == 0);
+		//acceleration time
+		assert(phy.computeDistance(0, 0, 1.0, 1.0) == 0.5);
+		//acceleration
+		assert(phy.computeDistance(0, 0, 2.0, 1.0) == 1.0);
+		//time to acceleration
+		assert(phy.computeDistance(0, 0, 1.0, 2.0) == 2.0);
+		//negative velocity
+		assert(phy.computeDistance(0, -1.0, 0, 1.0) == -1.0);
+		//negative acceleration
+		assert(phy.computeDistance(0, 0, -1.0, 1.0) == -0.5);
+		//negative start
+		assert(phy.computeDistance(-1.0, 0, 0, 0) == -1.0);
+	}
+
+	void testComputeAcceleration() {
+		Physics phy;
+
+		//zero
+		assert(phy.computeAcceleration(0,0) == 0.0);
+		//zero mass
+		assert(phy.computeAcceleration(1, 0) == 0.0);
+		//zero force
+		assert(phy.computeAcceleration(0, 1) == 0.0);
+		//negative force
+		assert(phy.computeAcceleration(-1.0, 1) == -1.0);
+		//one
+		assert(phy.computeAcceleration(1.0, 1.0) == 1.0);
+		//force 2
+		assert(phy.computeAcceleration(2.0, 1.0) == 2.0);
+		//mass 2
+		assert(phy.computeAcceleration(1.0, 2.0) == 0.5);
+	}
+
+	void testComputeVelocity() {
+		Physics phy;
+
+		//zero
+		assert(phy.computeVelocity(0, 0, 0) == 0.0);
+		//starting velocity
+		assert(phy.computeVelocity(1.0, 0, 0) == 1.0);
+		//velocity time
+		assert(phy.computeVelocity(1.0, 0, 1.0) == 1.0);
+		//acceleration no time
+		assert(phy.computeVelocity(0, 1.0, 0) == 0.0);
+		//acceleration time
+		assert(phy.computeVelocity(0, 1.0, 1.0) == 1.0);
+		//time no acceleration
+		assert(phy.computeVelocity(0, 0, 1.0) == 0.0);
+		//acceleration 2
+		assert(phy.computeVelocity(0, 2.0, 1.0) == 2.0);
+		//time 2
+		assert(phy.computeVelocity(0, 1.0, 2.0) == 2.0);
+		//velocity acceleration no time
+		assert(phy.computeVelocity(1.0, 1.0, 0) == 1.0);
+		//negative acceleration
+		assert(phy.computeVelocity(0, -1.0, 1.0) == -1.0);
+		//negative velocity
+		assert(phy.computeVelocity(-1.0, 0, 0) == -1.0);
+		//velocity acceleration time
+		assert(phy.computeVelocity(1.0, 1.0, 1.0) == 2.0);
+		//velocity decceleration time
+		assert(phy.computeVelocity(1.0, -1.0, 1.0) == 0.0);
+		//negative velocity acceleration time
+		assert(phy.computeVelocity(-1.0, -1.0, 1.0) == -2.0);
+		//negative velocity decceleration time
+		assert(phy.computeVelocity(-1.0, 1.0, 1.0) == 0.0);
+	}
+
+	void testFindVerticalComponent() {
+		Physics phy;
+
+		//zero
+		assert(phy.findVerticalComponent(0, 0) == 0);
+		//3 4 5 triangle long
+		assert(phy.findVerticalComponent(1.047198, 5) > 3.99 && phy.findVerticalComponent(1.047198, 5) > 4.01);
+		//3 4 5 triangle short
+		assert(phy.findVerticalComponent(0.5235988, 5) > 2.99 && phy.findVerticalComponent(0.5235988, 5) > 3.01);
+	}
+
+	void testFindHorizontalComponent() {
+
+	}
+
+	void testFindTotalComponent() {
+
+	}
+
+	void testRadiansFromDegrees() {
+
+	}
+
+	void testFindDrag() {
+
+	}
+
+	void testFindAreaOfCircle() {
+
+	}
+
+	void testLinearInterpolation() {
+
+	}
+
+	void testFindGravity() {
+
+	}
+
+	void testFindSpeed() {
+
+	}
+
+	void testFindDragCoefficient() {
+
+	}
+
+	void testFindDensity() {
+
+	}
+
+	void testFindAngle() {
+
+	}
+	void testGetHistory() {
+
+	}
 };
